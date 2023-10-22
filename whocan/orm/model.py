05021155dc25d80bf6f2b38d2bb276f4c1c85a5b
Brett Csotty <bcsotty@umich.edu>
2023-10-21 22:50:04 -0400

    Updated model

--- whocan/orm/model.py
+++ whocan/orm/model.py
@@ -6,13 +6,9 @@ from sqlmodel import SQLModel, Field
 from sqlalchemy import PrimaryKeyConstraint
 
 
-class County(SQLModel, table=True):
-    name: str = Field(primary_key=True)
-
-
 class Candidate(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True)
-    county: str = Field(foreign_key="county.name")
+    district: str
     name: str
 
 
